914ccbb09baae02050d9348627559819c6f1bff0
Rahma Mohammed <rahmaurur@gmail.com>
2023-09-30 12:55:07 +0100

    second commit

---
+++ loops.js
@@ -0,0 +1,42 @@
+for(let i =0; i<5; i++) {
+    console.log("Rahma")
+}
+
+
+let i = 0;
+
+while(i <10) {
+    console.log("Eahma ")
+    i++
+} 
+
+let today = "Sunday"
+
+if(today == "Saturday") {
+    console.log ("Come to Coding Class")
+} else if (today == "Sunday") {
+    console.log("Sleep")
+} else if( today == "Monday") {
+    console.log("Go to school")
+} else {
+    console.log("Do whatever you like")
+}
+
+
+switch (today) {
+    case "Saturday":
+        console.log("Coding time ...")
+        break;
+    case "Sunday":
+        console.log("Sleep time")
+        break;
+    case "Monday":
+        console.log("Monday")
+          break;
+
+
+    default:
+        console.log("Do whatever you like")
+        
+        break;
+}
