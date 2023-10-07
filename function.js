f3eaab59462cec52d1d5cbf292eb0a303ce0b127
Rahma Mohammed <rahmaurur@gmail.com>
2023-10-07 13:17:05 +0100

    I added a function

---
+++ function.js
@@ -0,0 +1,8 @@
+//write a simple JAVASCRIPT function called multiply that takes two numbers as params and returns the result to the caller
+
+function multiply(a,y) {
+    return a * y 
+}
+
+let result = multiply (5,8)
+console.log(result)
